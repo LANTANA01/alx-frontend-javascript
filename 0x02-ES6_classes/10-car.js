3ef482bb90be3361964ce02dca0b0592b84e0491
LANTANA01 <ojimahTijani@gmail.com>
2024-07-26 01:21:44 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+// Implement a class named Car
+export default class Car {
+  constructor(brand = null, motor = null, color = null) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const clone = new this.constructor();
+    const symbols = Object.getOwnPropertySymbols(this);
+    symbols.forEach((symbol) => {
+      clone[symbol] = this[symbol];
+    });
+    return clone;
+  }
+}
